1ad32a8183dde9b3d94dccd1486e543793782d76
Branko Culum <brankoculum@users.noreply.github.com>
2018-09-23 00:32:38 -0400

    Added button randomize notes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Monome from './Monome'
+import Monome from './Monome';
+import noteFrequencies from './notes.json'
 
 class App extends Component {
   ROWS = 8
@@ -22,6 +23,9 @@ class App extends Component {
   }
 
   createOscillator = (freq) => {
+    // Creates an oscillator locked to the input frequences,
+    // returns just the gainNode for volume manipulation
+
     let oscillator = this.state.audioCtx.createOscillator();
     let gainNode = this.state.audioCtx.createGain();
 
@@ -37,7 +41,6 @@ class App extends Component {
   }
 
   playNote = (gainId) => {
-    console.log('Playing.. ' + gainId)
     const gainNode = this.state.gainNodes[gainId];
 
     gainNode.gain.value = 1;
@@ -48,9 +51,28 @@ class App extends Component {
     }, 100)
   }
 
-  componentDidMount() {
+  generateNewNotes = () => {
+    let audioCtx = new AudioContext();
+
     let gainNodes = [];
-    let notes = [783.99, 659.25, 523.25, 440, 349.23, 293.66, 246.94, 196.00];
+    let nf = noteFrequencies;
+
+    // These notes sound nice
+    // let notes = [
+    //   nf['D5'], nf['C5'], nf['B4'], nf['A4'], nf['G4'], nf['F4'], nf['E4'], nf['D4'],
+    // ]
+
+    let notes = []
+
+    for (let i = 0; i < 8; i++) {
+      let possibleNotes = Object.keys(noteFrequencies)
+      let noteId = Math.floor(Math.random() * possibleNotes.length);
+      let note = possibleNotes[noteId];
+
+      notes.push(noteFrequencies[note])
+    }
+
+    notes = notes.sort((a, b) => b - a)
 
     for (let i = 0; i < this.ROWS * this.COLS; i++) {
       let noteId = Math.floor(i / this.COLS);
@@ -58,7 +80,11 @@ class App extends Component {
       gainNodes.push(this.createOscillator(notes[noteId]));
     }
 
-    this.setState({ gainNodes })
+    this.setState({ audioCtx, gainNodes })
+  }
+
+  componentDidMount() {
+    this.generateNewNotes()
 
     setInterval(() => {
       this.setState(prevState => ({ current: (prevState.current + 1) % this.COLS }))
@@ -80,6 +106,11 @@ class App extends Component {
           tiles={this.state.tileStates}
           toggleTile={this.toggleTile}
           current={this.state.current}
+          />
+        <input
+          type="button"
+          value="New Notes"
+          onClick={this.generateNewNotes}
           />
       </div>
     );
